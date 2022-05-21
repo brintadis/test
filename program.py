fb2198e3cea092fd1d51e2e1458369db1f2012cf
Makar Timofeev <makar.timofeev.1990@gmail.com>
2022-05-21 18:57:50 +0300

    working with files

--- program.py
+++ program.py
@@ -1,1 +1,10 @@
-print("Hello Makar!")
+with open('referat.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+    file_len = len(text)
+    file_words = len(text.split())
+    text = text.replace('.', '!')
+    print(file_len)
+    print(file_words)
+
+    with open('referat2.txt', 'w', encoding='utf-8') as file:
+        file.write(text)
