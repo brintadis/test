63a2ef580695ffb280b13dd0addc6240725e628b
Makar Timofeev <makar.timofeev.1990@gmail.com>
2022-05-21 19:37:17 +0300

    working with csv

---
+++ working-with-csv.py
@@ -0,0 +1,14 @@
+import csv
+
+user_list = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+    ]
+
+with open('user-list.csv', 'w', encoding='utf-8') as file:
+    fields = ['name', 'age', 'job']
+    writer = csv.DictWriter(file, fields, delimiter=';')
+    writer.writeheader()
+    for user in user_list:
+        writer.writerow(user)
